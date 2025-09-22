13b4281183712a5e94ad4ba4a27adb7e3a9a4b77
Kelvin Benson <135385524+vawickhq@users.noreply.github.com>
2025-09-23 01:30:35 +0200

    Fix React Router basename and clean up asset references

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/project-management-app">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
